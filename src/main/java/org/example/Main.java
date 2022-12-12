2c199379bd1c147847d73d43abe4415219b01293
Katerina Latysheva <katenych@MBP-Katerina.attlocal.net>
2022-12-11 22:32:35 -0800

    new repo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,29 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String team1 = "Cats";
+        int cat1 = 6;
+        int cat2 = 13;
+        int cat3 = 9;
+        int cat4 = 32;
+        int cat5 = 10;
+        int CatsResult = (cat1+cat2+cat3+cat4+cat5);
+
+        String team2 = "Dogs";
+        int dog1 = 12;
+        int dog2 = 31;
+        int dog3 = 5;
+        int dog4 = 19;
+        int dog5 = 19;
+        int DogsResult = (dog1+dog2+dog3+dog4+dog5);
+
+        if (CatsResult>DogsResult) {
+            System.out.println("Перемогла команда " + team1 + " - набрала " + CatsResult + " очків");
+        } else {
+            System.out.println("Перемогла команда " + team2 + " - набрала " + DogsResult + " очків");
+        }
+
     }
 }
